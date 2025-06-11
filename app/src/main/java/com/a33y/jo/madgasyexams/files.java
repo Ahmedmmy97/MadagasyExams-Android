3b6874c098d9be9e67c86734c99b228c9cad8415
ayousef <ayousef@fdnsoft.com>
2025-06-11 14:21:57 +0300

    Refactor code for improved readability and consistency

--- app/src/main/java/com/a33y/jo/madgasyexams/files.java
+++ app/src/main/java/com/a33y/jo/madgasyexams/files.java
@@ -1,17 +1,13 @@
 package com.a33y.jo.madgasyexams;
 
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -19,7 +15,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.LinearLayout;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.a33y.jo.madgasyexams.Adapters.RecyclerAdapter;
@@ -34,9 +29,6 @@ import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.io.File;
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class files extends Fragment implements ListItemClickListener, DataHelper.DataChangedListener {
@@ -50,10 +42,10 @@ public class files extends Fragment implements ListItemClickListener, DataHelper
     private int file_pos;
     private LinearLayout loading;
     InterstitialAd mInterstitialAd;
+
     public files() {
         // Required empty public constructor
     }
-
 
 
     @Override
@@ -68,14 +60,14 @@ public class files extends Fragment implements ListItemClickListener, DataHelper
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_files, container, false);
+        View view = inflater.inflate(R.layout.fragment_files, container, false);
         //requestNewInterstitial(0);
         getActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
         initAd(view);
         recyclerView = view.findViewById(R.id.recycler);
         loading = view.findViewById(R.id.progressbar);
-        RecyclerAdapter adapter = new RecyclerAdapter(getActivity(), subject.getFileNames(),this);
-        layoutManager = new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL,false);
+        RecyclerAdapter adapter = new RecyclerAdapter(getActivity(), subject.getFileNames(), this);
+        layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
         recyclerView.setExpanded(true);
@@ -91,83 +83,84 @@ public class files extends Fragment implements ListItemClickListener, DataHelper
         AdRequest adRequest = new AdRequest.Builder()
                 .build();
 
-        if(Connectivity.isNetworkAvailable(getContext()))
-            InterstitialAd.load(getContext(),"ca-app-pub-7038787403547669/5022810596", adRequest,
-                new InterstitialAdLoadCallback() {
-                    @Override
-                    public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
-                        // The mInterstitialAd reference will be null until
-                        // an ad is loaded.
-                        mInterstitialAd = interstitialAd;
-                        mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback() {
-                            @Override
-                            public void onAdDismissedFullScreenContent() {
-                                // Called when fullscreen content is dismissed.
+        if (Connectivity.isNetworkAvailable(getContext()))
+            InterstitialAd.load(getContext(), "ca-app-pub-7038787403547669/5022810596", adRequest,
+                    new InterstitialAdLoadCallback() {
+                        @Override
+                        public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                            // The mInterstitialAd reference will be null until
+                            // an ad is loaded.
+                            mInterstitialAd = interstitialAd;
+                            mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback() {
+                                @Override
+                                public void onAdDismissedFullScreenContent() {
+                                    // Called when fullscreen content is dismissed.
 
-                                navigate(position);
-                                loading.setVisibility(View.GONE);
-                                Log.d("Ads", "The ad was dismissed.");
-                            }
+                                    navigate(position);
+                                    loading.setVisibility(View.GONE);
+                                    Log.d("Ads", "The ad was dismissed.");
+                                }
 
-                            @Override
-                            public void onAdFailedToShowFullScreenContent(AdError adError) {
-                                // Called when fullscreen content failed to show.
+                                @Override
+                                public void onAdFailedToShowFullScreenContent(AdError adError) {
+                                    // Called when fullscreen content failed to show.
 
-                                navigate(position);
-                                loading.setVisibility(View.GONE);
-                                Log.d("Ads", "The ad failed to show.");
-                            }
+                                    navigate(position);
+                                    loading.setVisibility(View.GONE);
+                                    Log.d("Ads", "The ad failed to show.");
+                                }
 
-                            @Override
-                            public void onAdShowedFullScreenContent() {
-                                // Called when fullscreen content is shown.
-                                // Make sure to set your reference to null so you don't
-                                // show it a second time.
-                                mInterstitialAd = null;
-                                Log.d("Ads", "The ad was shown.");
-                            }
-                        });
-                        mInterstitialAd.show(getActivity());
-                        Log.i("Ads", "onAdLoaded");
-                    }
+                                @Override
+                                public void onAdShowedFullScreenContent() {
+                                    // Called when fullscreen content is shown.
+                                    // Make sure to set your reference to null so you don't
+                                    // show it a second time.
+                                    mInterstitialAd = null;
+                                    Log.d("Ads", "The ad was shown.");
+                                }
+                            });
+                            mInterstitialAd.show(getActivity());
+                            Log.i("Ads", "onAdLoaded");
+                        }
 
-                    @Override
-                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
-                        // Handle the error
-                        Log.i("Ads", loadAdError.getMessage());
-                        mInterstitialAd = null;
-                        loading.setVisibility(View.GONE);
-                        navigate(position);
-                    }
-                });
+                        @Override
+                        public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                            // Handle the error
+                            Log.i("Ads", loadAdError.getMessage());
+                            mInterstitialAd = null;
+                            loading.setVisibility(View.GONE);
+                            navigate(position);
+                        }
+                    });
         else {
             loading.setVisibility(View.GONE);
             navigate(position);
         }
 
     }
-    private void navigate(int position){
+
+    private void navigate(int position) {
         loading.setVisibility(View.GONE);
-        Intent intent = new Intent(getContext(),PdfViewer.class);
-        intent.putExtra("subject",subject);
-        intent.putExtra("pos",position);
+        Intent intent = new Intent(getContext(), PdfViewer.class);
+        intent.putExtra("subject", subject);
+        intent.putExtra("pos", position);
         startActivity(intent);
     }
-    boolean CheckFile(Subject s,int pos, boolean isAns){
-        for(File f : s.getFiles()) {
-            if(!isAns)
+
+    boolean CheckFile(Subject s, int pos, boolean isAns) {
+        for (File f : s.getFiles()) {
+            if (!isAns)
                 if (f.getName().equals(s.getFileNames().get(pos).getFileName())) {
                     file_pos = s.getFiles().indexOf(f);
                     return true;
-                }
-            else
-                if (f.getName().equals(s.getFileNames().get(pos).getFileNameAns())) {
+                } else if (f.getName().equals(s.getFileNames().get(pos).getFileNameAns())) {
                     file_pos = s.getFiles().indexOf(f);
                     return true;
                 }
         }
         return false;
     }
+
     @Override
     public void onClick(int pos) {
 
@@ -179,18 +172,18 @@ public class files extends Fragment implements ListItemClickListener, DataHelper
         builder.setItems(choices, (dialog, which) -> {
             // the user clicked on colors[which]
 
-            switch (which){
+            switch (which) {
                 case 0:
-                    if (!CheckFile(subject, pos,false)){
-                        if(Connectivity.isNetworkAvailable(getContext())) {
+                    if (!CheckFile(subject, pos, false)) {
+                        if (Connectivity.isNetworkAvailable(getContext())) {
                             loading.setVisibility(View.VISIBLE);
-                            DataHelper.DownloadRemFile(subject, subject.getFileNames().get(pos).getFileName(),false, getContext(),loading,files.this);
-                        }else {
+                            DataHelper.DownloadRemFile(subject, subject.getFileNames().get(pos).getFileName(), false, getContext(), loading, files.this);
+                        } else {
                             Snackbar bar = Snackbar.make(getView(), "Il n'y a pas de connexion internet!", Snackbar.LENGTH_LONG);
-                            bar.getView().setBackgroundColor(ContextCompat.getColor(getContext(),R.color.colorPrimaryDark));
+                            bar.getView().setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorPrimaryDark));
                             bar.show();
                         }
-                    }else {
+                    } else {
                         loading.setVisibility(View.VISIBLE);
                         requestNewInterstitial(file_pos);
 
@@ -198,16 +191,16 @@ public class files extends Fragment implements ListItemClickListener, DataHelper
                     }
                     break;
                 case 1:
-                    if (!CheckFile(subject, pos, true)){
-                        if(Connectivity.isNetworkAvailable(getContext())) {
+                    if (!CheckFile(subject, pos, true)) {
+                        if (Connectivity.isNetworkAvailable(getContext())) {
                             loading.setVisibility(View.VISIBLE);
-                            DataHelper.DownloadRemFile(subject, subject.getFileNames().get(pos).getFileNameAns(),true, getContext(),loading,files.this);
-                        }else {
+                            DataHelper.DownloadRemFile(subject, subject.getFileNames().get(pos).getFileNameAns(), true, getContext(), loading, files.this);
+                        } else {
                             Snackbar bar = Snackbar.make(getView(), "Il n'y a pas de connexion internet!", Snackbar.LENGTH_LONG);
-                            bar.getView().setBackgroundColor(ContextCompat.getColor(getContext(),R.color.colorPrimaryDark));
+                            bar.getView().setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorPrimaryDark));
                             bar.show();
                         }
-                    }else {
+                    } else {
                         loading.setVisibility(View.VISIBLE);
                         requestNewInterstitial(file_pos);
 
@@ -221,7 +214,8 @@ public class files extends Fragment implements ListItemClickListener, DataHelper
         builder.show();
 
     }
-    void initAd(View v){
+
+    void initAd(View v) {
         MobileAds.initialize(getContext());
         AdView mAdView = v.findViewById(R.id.adView);
         AdRequest request = new AdRequest.Builder()
@@ -229,6 +223,7 @@ public class files extends Fragment implements ListItemClickListener, DataHelper
                 .build();
         mAdView.loadAd(request);
     }
+
     @Override
     public void onDataAdded() {
 
@@ -245,9 +240,9 @@ public class files extends Fragment implements ListItemClickListener, DataHelper
     @Override
     public void onFileDownloadedfailed(boolean isAns) {
         loading.setVisibility(View.GONE);
-        if (isAns){
+        if (isAns) {
             Snackbar bar = Snackbar.make(getView(), "il n'y a pas de corriger disponibles.", Snackbar.LENGTH_LONG);
-            bar.getView().setBackgroundColor(ContextCompat.getColor(getContext(),R.color.colorRed));
+            bar.getView().setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorRed));
             bar.show();
         }
 
